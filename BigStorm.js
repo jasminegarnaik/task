7365bb472196cb94e66b65eef557e1777146982c
jasminegarnaik <141756116+jasminegarnaik@users.noreply.github.com>
2023-12-08 01:16:05 +0530

    js added

---
+++ BigStorm.js
@@ -0,0 +1,22 @@
+let name1=document.getElementById('name')
+        let password=document.getElementById('password')
+        let demo=document.getElementById('demo')
+        function passwordValidate(){
+            let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
+            if(strongPassword.test(password.value)) {
+                return true;
+            }
+            else{
+                return false;
+            }
+            }
+        
+        function signIn(){
+        if(passwordValidate()){
+            demo.innerHTML="Sign in successful!!! Welcome "+ name1.value +" Learn More with BigStorm"
+        }
+        else
+        demo.innerHTML="Please enter a valid password(password should be of minimum 8 characters having atleast 1 small letters(a-z),1 capital letters(A-Z) ,1 digit(0-9),1 character)"
+        
+    }
+   
